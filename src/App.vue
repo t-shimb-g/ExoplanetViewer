9a8dad8fce15f7ebdcc011fa8800f8523c331ab4
Tyler Guldberg <tylgul89537@gmail.com>
2026-02-06 09:46:36 -0600

    Experimenting with Vuetify

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const routes = {
 }
 
 const currentPath = ref(window.location.hash)
+const drawer = ref(true);
 
 window.addEventListener('hashchange', () => {
     currentPath.value = window.location.hash
@@ -21,8 +22,28 @@ const currentView = computed(() => {
 </script>
 
 <template>
-    <a href="#/">Home</a> |
-    <a href="#/about">About</a>
-
-    <component :is="currentView"></component>
+    <v-app>
+        <v-navigation-drawer color="blue" v-model="drawer">
+            <v-list-item
+                prepend-icon="mdi-home"
+                href="#/"
+                title="Home"
+                @click="drawer = !drawer"
+            ></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-information-variant-circle-outline"
+                href="#/about"
+                title="About Us"
+                @click="drawer = !drawer"
+            ></v-list-item>
+        </v-navigation-drawer>
+        <v-app-bar>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-title>Exoplanet Explorer</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <component :is="currentView"></component>
+        </v-main>
+        <v-footer app="true">Copyright 2026</v-footer>
+    </v-app>
 </template>
